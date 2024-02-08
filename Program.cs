8af9a9d4213b4661e66118c24e8ed4e2d872ccb0
408b7f8b <davbr@tutanota.com>
2024-02-08 16:27:16 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Linq;
+using System.Collections.Generic;
+using isci.Allgemein;
+using isci.Daten;
+using isci.Beschreibung;
+using Newtonsoft.Json.Linq;
+
+namespace isci.monitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Erstellung des Zugriffs auf die dateibasierte Datenstruktur unter Nutzung der Parametrierung.
+            var structure = new Datenstruktur("/media/ramdisk", "monitor", "Demonstrator");
+
+            //Hinzufügen aller als Dateien gespeicherte Datenmodelle im Standardordner.
+            structure.DatenmodelleEinhängenAusOrdner("/OrdnerDatenmodelle");
+            //Logischer Start der Datenstruktur.
+            structure.Start();
+            
+            //Arbeitsschleife
+            while(true)
+            {
+                System.Console.Clear();
+                structure.Lesen();
+
+                foreach (var eintrag in structure.dateneinträge)
+                {
+                    System.Console.WriteLine(eintrag.Key + ": " + eintrag.Value.WertSerialisieren());
+                }
+
+                System.Threading.Thread.Sleep(1000);                 
+            }
+        }
+    }
+}
